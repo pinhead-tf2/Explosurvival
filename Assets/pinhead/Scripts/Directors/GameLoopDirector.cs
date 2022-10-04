4d335d44fe965fc0dfa942ff3216ae7cf1c26937
pinhead <mrmaxc2023@gmail.com>
2022-10-04 18:38:57 -0500

    Learning Rider

--- Assets/pinhead/Scripts/Directors/GameLoopDirector.cs
+++ Assets/pinhead/Scripts/Directors/GameLoopDirector.cs
@@ -1,76 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
 
-public class GameLoopDirector : MonoBehaviour
-{ 
-    [Header("Game States")]
-    [ReadOnly] public string gameState = "NoState";
-    private string[] validGameStates = new string[] {"NoState", "Intermission", "Setup", "Playing", "Cleanup"};
-    [Space(5)]
+namespace pinhead.Scripts.Directors
+{
+    public class GameLoopDirector : MonoBehaviour
+    { 
+        [Header("Game States")]
+        [ReadOnly] public string gameState = "NoState";
+        private readonly string[] _validGameStates = new string[] {"NoState", "Intermission", "Setup", "Playing", "Cleanup"};
+        [Space(5)]
 
-    [Header("Timer")] // Timer Section
-    [Tooltip("The time left in the current intermission or round.")]
-    [ReadOnly] public float timeLeft;
-    [ReadOnly] public bool timerActive = false;
-    private float intermissionLength = 30f; // Length of the intermission (seconds)
-    private float roundLength = 150f; // Length of a round (seconds)
-    [Space(5)]
+        [Header("Timer")] // Timer Section
+        [Tooltip("The time left in the current intermission or round.")]
+        [ReadOnly] public float timeLeft;
+        [ReadOnly] public bool timerActive;
+        private readonly float _intermissionLength = 30f, _roundLength = 150f; // Length of the intermission, round (seconds)
+        [Space(5)]
 
-    [Header("UI")]
-    [SerializeField] TextMeshProUGUI timerText;
+        [Header("UI")]
+        [SerializeField] TextMeshProUGUI timerText;
 
-    // Code
+        // Code
 
-    void Start() {
-        Intermission();
-    }
+        private void Start() {
+            Intermission();
+        }
 
-    void Update() {
-        if (timerActive) {
-            if (timeLeft > 0) {
-                timeLeft -= Time.deltaTime;
-                // Update UI
-            } else {
-                timerActive = false;
-                timeLeft = 0f;
-                if (gameState == validGameStates[1]) { // Intermission
-                    SetupPlayspace();
-                } else if (gameState == validGameStates[3]) { // Playing
-                    EndRound();
+        private void Update() {
+            if (timerActive) {
+                if (timeLeft > 0) {
+                    timeLeft -= Time.deltaTime;
+                    // Update UI
+                } else {
+                    timerActive = false;
+                    timeLeft = 0f;
+                    if (gameState == _validGameStates[1]) { // Intermission
+                        SetupPlayspace();
+                    } else if (gameState == _validGameStates[3]) { // Playing
+                        EndRound();
+                    }
                 }
             }
         }
+
+        // Custom Voids
+
+        private void Intermission() {
+            gameState = _validGameStates[1]; // Intermission
+            timeLeft = _intermissionLength;
+            timerActive = true;
+        }
+
+        private void SetupPlayspace() { // Possibly make this a game setup director??
+            gameState = _validGameStates[2]; // Setup
+            // do stuff
+
+            // countdown
+            GameLoop();
+        }
+
+        private void GameLoop() {
+            gameState = _validGameStates[3]; // Playing
+            // Make bomb and item directors active
+            EndRound();
+        }
+
+        private void EndRound() {
+            gameState = _validGameStates[4]; // Cleanup
+
+            Intermission();
+        }
+
     }
-
-    // Custom Voids
-
-    void Intermission() {
-        gameState = validGameStates[1]; // Intermission
-        timeLeft = intermissionLength;
-        timerActive = true;
-    }
-
-    void SetupPlayspace() { // Possibly make this a game setup director??
-        gameState = validGameStates[2]; // Setup
-        // do stuff
-
-        // countdown
-        GameLoop();
-    }
-
-    void GameLoop() {
-        gameState = validGameStates[3]; // Playing
-        // Make bomb and item directors active
-        EndRound();
-    }
-
-    void EndRound() {
-        gameState = validGameStates[4]; // Cleanup
-
-        Intermission();
-    }
-
 }
